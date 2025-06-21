5a33a1e5a17b8abf1a1c53247b809183bae657e1
jered wong <jeredwong711@gmail.com>
2025-06-21 23:34:19 +0800

    feat: perform GET request on word provided as CLI argument
    feat: perform GET request on word provided as CLI argument
    
    feat: perform GET request on word provided as CLI argument

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/jeredwong/wordup
 
 go 1.22.3
-
-require github.com/go-sql-driver/mysql v1.6.0
--- main.go
+++ main.go
@@ -2,37 +2,84 @@ package main
 
 import (
 	"fmt"
+	"os"
+	// "os/exec"
+	"encoding/json"
+	"io"
 	"log"
-	"database/sql"
-	"github.com/go-sql-driver/mysql"
+	"net/http"
 )
 
 func main() {
+	const BasePath = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 
- 	var db *sql.DB
- 	// connection properties
- 	config := mysql.Config {
- 		User:	"wordup-admin",
- 		Passwd:	"wordup-admin",
- 		Net:	"tcp",
- 		Addr:	"127.0.0.1:3306",
-		DBName: "wordup_wordbank",
-		AllowNativePasswords: true,
- 	}
-
-	// database handle
-	var err error
-	db, err = sql.Open("mysql", config.FormatDSN())
-	if err != nil {
-		log.Fatal(err)
+	// Structs 
+	type Definition struct {
+		Definition string
 	}
 
-	pingErr := db.Ping()
-	if pingErr != nil {
-		log.Fatal(pingErr)
+	type Meaning struct {
+		Definitions []Definition
 	}
 
-	fmt.Println("connection established!")
+	type Word struct {
+		Word string 
+		Meanings []Meaning
+	}
 
+	// Retrieve word definition
+	if len(os.Args) == 2 {
+		word := os.Args[1]
+
+		// log.Printf("INFO: making GET request for word \"%s\"\n", word)
+
+		// Making GET request
+		resp, err := http.Get(BasePath + word)
+		if err != nil { 
+			log.Fatalf("Error making GET request: %v", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode == 404 {
+			log.Fatalf("Definition for \"%s\" not found", word)
+		}
+
+		if resp.StatusCode != 200 {
+			log.Fatalf("Status Code not 200: %d", resp.StatusCode)
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalf("Error reading response body: %v", err)
+		}
+
+		// Unmarshalling response
+		var words []Word
+		err = json.Unmarshal(body, &words)
+		if err != nil {
+			log.Fatalf("Error unmarshalling response body: %v", err)
+		}
+
+		fmt.Printf("word: %v\n", words[0].Word)
+
+		fmt.Println("definitions:")
+		for i, meaning := range words[0].Meanings {
+			fmt.Printf("(%d) %v\n",i+1 , meaning.Definitions[0].Definition)
+		}
+	} else if len(os.Args) == 1 {
+		fmt.Println("attempting to show word bank...")
+	// 	// path, err := exec.LookPath("open-wordbank")
+	// 	// if err != nil {
+	// 	// log.Fatal("error when finding script to open wordbank: %s", err)
+	// 	// }
+	// 	// fmt.Printf("script is available at %s\n", path)
+	// 	cmd := exec.Command("/bin/sh", "open-wordbank.sh")
+	// 	cmd.Stdout = os.Stdout
+	// 	cmd.Stdin = os.Stdin
+	// 	cmd.Stderr = os.Stderr
+	// 	fmt.Println("printed out wordbank")
+	} else {
+		fmt.Println("Invalid arguments provided")
+	}
 
 }
